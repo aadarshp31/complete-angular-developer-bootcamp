acb1da23263605b2fb42725994f8ef7f624025f8
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-11 21:32:17 +0530

    Completed Counter App.

--- counter-app/src/app/app.component.ts
+++ counter-app/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter App';
+  count: number = 0;
+
+  handleIncrement = () => {
+    this.count<10 ? this.count = this.count + 1 : "";
+  }
+
+  handleDecrement = () => {
+    this.count>0 ? this.count = this.count - 1 : "";
+  }
+
+  handleReset = () => {
+    this.count = 0;
+  }
 }
