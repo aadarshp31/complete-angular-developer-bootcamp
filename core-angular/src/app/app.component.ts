76bb2820c202c2f1ca92edd7ddb95090a6a489c4
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-12 23:42:48 +0530

    Created my first Angular Component.

---
+++ core-angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "my-app",
+  template: `<h1>Hey Angular</h1>`,
+  styles: [`
+    h1{
+      color: #fff;
+    }
+  `]
+})
+export class AppComponent { }
