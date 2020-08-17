6e68023aac673fd46b7207b0624fcee315ead774
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-17 19:53:19 +0530

    Separated HTML and CSS for our component into separate files.

--- core-angular/src/app/app.component.ts
+++ core-angular/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from "@angular/core"
 
 @Component({
   selector: "my-app",
-  template: `<h1>Hey Angular</h1>`,
-  styles: [`
-    h1{
-      color: #fff;
-    }
-  `]
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent { }
