1af7f6c075786f54856fed255eef9d5a9c674e49
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-29 23:00:26 +0530

    Imported and configured 'ngx-toastr' module in App.module .
    Also imported 'BrowserAnimationsModule' from '@angular/platform-browser/animations' as ngx-toastr requires it to work properly.

--- tic-tac-toe/src/app/app.module.ts
+++ tic-tac-toe/src/app/app.module.ts
@@ -4,7 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconComponent } from './components/icon/icon.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+// Fontawesome
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// Toaster
+import { ToastrModule } from "ngx-toastr";
 
 @NgModule({
   declarations: [
@@ -14,7 +18,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
