02a93604c931fe9ed0eee4944703d4212ca5148b
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-30 00:35:11 +0530

    Implimented the winning logic for the game with the method 'checkIsWinner'.

--- tic-tac-toe/src/app/app.component.ts
+++ tic-tac-toe/src/app/app.component.ts
@@ -14,6 +14,61 @@ export class AppComponent {
 
   constructor(private toastr: ToastrService) {}
 
+  checkIsWinner = () => {
+    
+    if(
+      this.itemArray[0] === this.itemArray[1] &&
+      this.itemArray[0] === this.itemArray[2] &&
+      this.itemArray[0] !== 'empty'
+    ){
+      this.winMessage = `${this.itemArray[0]} won!`
+    } else if(
+      this.itemArray[3] === this.itemArray[4] &&
+      this.itemArray[3] === this.itemArray[5] &&
+      this.itemArray[3] !== 'empty'
+    ){
+      this.winMessage = `${this.itemArray[3]} won!`
+    } else if(
+      this.itemArray[6] === this.itemArray[7] &&
+      this.itemArray[6] === this.itemArray[8] &&
+      this.itemArray[6] !== 'empty'
+    ){
+      this.winMessage = `${this.itemArray[6]} won!`
+    } else if(
+      this.itemArray[0] === this.itemArray[3] &&
+      this.itemArray[0] === this.itemArray[6] &&
+      this.itemArray[0] !== 'empty'
+    ){
+      this.winMessage = `${this.itemArray[0]} won!`
+    } else if(
+      this.itemArray[1] === this.itemArray[4] &&
+      this.itemArray[1] === this.itemArray[7] &&
+      this.itemArray[1] !== 'empty'
+    ){
+      this.winMessage = `${this.itemArray[1]} won!`
+    } else if(
+      this.itemArray[2] === this.itemArray[5] &&
+      this.itemArray[2] === this.itemArray[8] &&
+      this.itemArray[2] !== 'empty'
+    ){
+      this.winMessage = `${this.itemArray[2]} won!`
+    } else if(
+      this.itemArray[0] === this.itemArray[4] &&
+      this.itemArray[0] === this.itemArray[8] &&
+      this.itemArray[0] !== 'empty'
+    ){
+      this.winMessage = `${this.itemArray[0]} won!`
+    } else if(
+      this.itemArray[2] === this.itemArray[4] &&
+      this.itemArray[2] === this.itemArray[6] &&
+      this.itemArray[2] !== 'empty'
+    ){
+      this.winMessage = `${this.itemArray[2]} won!`
+    }
+
+
+  }
+
   reloadGame = () => {
     this.winMessage = '';
     this.isCross = false;
