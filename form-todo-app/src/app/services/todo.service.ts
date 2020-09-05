521e3873df66792fa0e09b6be780484c5e59c4b7
aadarshp31 <adarshpandey.in@gmail.com>
2020-09-05 23:08:55 +0530

    Created service to handle CRUD in todo.

--- form-todo-app/src/app/services/todo.service.ts
+++ form-todo-app/src/app/services/todo.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
-
+import { of } from "rxjs";
+import { Todo } from "../model/Todo";
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todos: Todo[];
 
-  constructor() { }
+  constructor() {
+    this.todos = [
+      {
+        id: "111",
+        title: "Learn C++",
+        isCompleted: true,
+        date: new Date()
+      },
+      {
+        id: "112",
+        title: "Learn React",
+        isCompleted: true,
+        date: new Date()
+      },
+      {
+        id: "113",
+        title: "Learn Angular",
+        isCompleted: false,
+        date: new Date()
+      },
+    ];
+  }
+
+  // Read
+  getTodos(){
+    return of(this.todos);
+  }
+
+  // Create
+  addTodo(todo: Todo){
+    this.todos.push(todo);
+  }
+
+  // Update
+  changeStatus(todo: Todo){
+    this.todos.map(currentTodo => {
+      if(currentTodo.id === todo.id){
+        return currentTodo.isCompleted = !currentTodo.isCompleted;
+      }
+    })
+  }
+
+  // Delete
+  deleteTodo(todo: Todo){
+    const indexOfTodo = this.todos.findIndex(currentTodo => currentTodo.id === todo.id);
+    this.todos.splice(indexOfTodo, 1);
+  }
+
 }
