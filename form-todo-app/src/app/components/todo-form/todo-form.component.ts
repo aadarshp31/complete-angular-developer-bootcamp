de7e45a62751fe5c7f281b33a51109179b670ce2
aadarshp31 <adarshpandey.in@gmail.com>
2020-09-07 16:47:22 +0530

    Created a 'handleAddTodo' method in todo-form component.

--- form-todo-app/src/app/components/todo-form/todo-form.component.ts
+++ form-todo-app/src/app/components/todo-form/todo-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../model/Todo';
+import { TodoService } from '../../services/todo.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-todo-form',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-form.component.css']
 })
 export class TodoFormComponent implements OnInit {
+  title: String;
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
+  }
+
+  handleAddTodo() {
+    const newTodo: Todo = {
+      id: uuidv4(),
+      title: this.title,
+      isCompleted: false,
+      date: new Date()
+    }
+
+    this.todoService.addTodo(newTodo);
+    this.title = "";
   }
 
 }
