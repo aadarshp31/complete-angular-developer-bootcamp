4c81789570d3617004c7185bc197e648226cb04e
aadarshp31 <adarshpandey.in@gmail.com>
2020-10-27 18:06:02 +0530

    Added ReactiveForms to our app
    Implimenting Reactive form inside our app.component.ts file by using bulit in 'Validators'
    Also added onReset and onSubmit methods to handle form reset and submit

--- signup-reactive/src/app/app.component.ts
+++ signup-reactive/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'signup-reactive';
+export class AppComponent implements OnInit {
+  title = 'signup-reactive'
+
+  signupForm: FormGroup
+  submitted = false
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      acceptTandC: [false, Validators.requiredTrue]
+    })
+  }
+
+  onSubmit() {
+    this.submitted = true
+    if (this.signupForm.invalid) {
+      return
+    }
+
+    console.table(this.signupForm.value)
+    console.table(this.signupForm)
+
+    console.log("Success Signup\n" + JSON.stringify(this.signupForm.value));
+  }
+
+  onReset() {
+    this.submitted = false
+    this.signupForm.reset()
+  }
 }
