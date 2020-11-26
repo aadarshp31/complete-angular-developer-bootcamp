99a4b4605fb50ebdd2b678d7bfa29db78ef87425
aadarshp31 <adarshpandey.in@gmail.com>
2020-11-27 00:29:00 +0530

    Added depenedencies to the app.module.ts

--- random-card/src/app/app.module.ts
+++ random-card/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+// Http Client
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+
+import { ToastrModule } from 'ngx-toastr'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { CardComponent } from './card/card.component';
     CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
