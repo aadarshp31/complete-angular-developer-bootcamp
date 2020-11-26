0a996a9645141981a5b61ac51664f7bb075801ce
aadarshp31 <adarshpandey.in@gmail.com>
2020-11-27 00:55:23 +0530

    Configured 'app.component.ts' to make an API call with hook 'OnInit' and services

--- random-card/src/app/app.component.ts
+++ random-card/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { UserService } from 'src/app/services/user.service'
+import { ToastrService } from 'ngx-toastr'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'random-card';
+export class AppComponent implements OnInit {
+  title = 'random-card'
+  user: any
+
+  constructor(private userService: UserService, private toastr: ToastrService) { }
+
+  ngOnInit() {
+    this.userService.getUser().subscribe(
+      (user: any) => {
+        console.log(user);
+        this.user = user.results[0]
+      },
+      (error) => {
+        this.toastr.error(error.status, "Something went wrong!")
+      }
+    )
+  }
 }
