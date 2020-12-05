a007cbe639e2a7fd10b42cf505a016fdd2397927
aadarshp31 <adarshpandey.in@gmail.com>
2020-12-05 20:29:40 +0530

    Added method to handle signout and fetch user email as soon as the component object is created

--- github-login-firebase/src/app/layouts/header/header.component.ts
+++ github-login-firebase/src/app/layouts/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  email: any = null
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    this.auth.getUser().subscribe(user => {
+      this.email = user?.email
+    })
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  async handleSignout() {
+    try {
+      await this.auth.signout()
+      this.router.navigateByUrl('/signin')
+      this.toastr.info('Sign in again to continue')
+      this.email = null
+    } catch (error) {
+      this.toastr.error('Something went wrong')
+    }
   }
 
 }
