f4b9361df8302ac5866320b9a6082eeb6d0b06d1
aadarshp31 <adarshpandey.in@gmail.com>
2020-12-06 00:57:42 +0530

    Subscribing to the GithubService to fetch data with OnChanges() lifecycle hook
    Also added ChnageDetectorRef service to detect change as a grandchild for the GithubSrvice

--- github-login-firebase/src/app/components/repos/repos.component.ts
+++ github-login-firebase/src/app/components/repos/repos.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ChangeDetectorRef } from '@angular/core';
+import { GithubService } from 'src/app/services/github.service'
 
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
   styleUrls: ['./repos.component.scss']
 })
-export class ReposComponent implements OnInit {
+export class ReposComponent implements OnInit, OnChanges {
+  @Input() repoUrl: string = ''
+  repos: any[] = []
 
-  constructor() { }
+  constructor(private githubService: GithubService, private changeRef: ChangeDetectorRef) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  ngOnChanges(): void {
+    if (this.repoUrl) {
+      this.githubService.getUserRepo(this.repoUrl).subscribe(
+        (repos: any) => {
+          this.repos = repos
+          this.changeRef.detectChanges()
+        },
+        (error) => { console.error(error) }
+      )
+    }
   }
 
 }
