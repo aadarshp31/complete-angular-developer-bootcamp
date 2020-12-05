fa67a9c730007a4648cbca0dbc59b94d24d25442
aadarshp31 <adarshpandey.in@gmail.com>
2020-12-05 19:25:12 +0530

    Added an suth service using '@angular/fire/auth' service
    Added methods to signin, signup, signout and getUserDetails

--- github-login-firebase/src/app/services/auth.service.ts
+++ github-login-firebase/src/app/services/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
+
+  // Returns and obervable
+  signup(email: string, password: string) {
+    return this.auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  // Returns and obervable
+  signin(email: string, password: string) {
+    return this.auth.signInWithEmailAndPassword(email, password)
+  }
+
+  // Get user deails when user is successfully logged in
+  getUser() {
+    return this.auth.authState
+  }
+
+  signout() {
+    return this.auth.signOut;
+  }
+
 }
