df4d19d6223dc7b8e219fbb64371846d4f34f9bf
aadarshp31 <adarshpandey.in@gmail.com>
2020-12-05 19:50:50 +0530

    Added search user and user's repo services using githubApi

--- github-login-firebase/src/app/services/github.service.ts
+++ github-login-firebase/src/app/services/github.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
+
+// https://api.github.com/users/aadarshp31
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserByUserName(userName: string) {
+    return this.http.get(`https://api.github.com/users/${userName}`)
+  }
+
+  getUserRepo(repoUrl: string) {
+    return this.http.get(repoUrl)
+  }
+
 }
