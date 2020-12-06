07a7a0d4125f2986f6f38222d78cd055e76ce8c7
aadarshp31 <adarshpandey.in@gmail.com>
2020-12-06 23:05:33 +0530

    Added onSubmit method for the NgForm and consuming auth service we created for firebase signup

--- github-login-firebase/src/app/pages/signup/signup.component.ts
+++ github-login-firebase/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
+
+import { Router } from '@angular/router'
+import { ToastrService } from 'ngx-toastr'
+import { AuthService } from 'src/app/services/auth.service'
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    const { email, password } = f.form.value
+    // TODO: Do your validation here...
+
+
+    this.auth.signup(email, password)
+      .then(res => {
+        this.router.navigateByUrl('/')
+        this.toastr.success('Signup Success')
+      })
+      .catch(err => {
+        console.error(err)
+        this.toastr.error(`Signup failed! \n${err.message}`)
+      })
   }
 
 }
