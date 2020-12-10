6f7bdd29495ffacbd9fa26abbee4343e5a908a0e
aadarshp31 <adarshpandey.in@gmail.com>
2020-12-10 18:26:18 +0530

    Added UI and functionality to Signin Component

--- github-login-firebase/src/app/pages/signin/signin.component.ts
+++ github-login-firebase/src/app/pages/signin/signin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    const { email, password } = f.form.value
+
+    // Validations goes here
+    if (!f.form.valid) {
+      throw this.toastr.error('Please fill all the fields carefully', 'Validation failed!')
+    }
+
+    this.auth.signin(email, password)
+      .then(res => {
+        this.router.navigateByUrl('/')
+        console.log(res);
+        this.toastr.success('Signin Successfull!')
+      })
+      .catch(error => {
+        console.error(error);
+        this.toastr.error("Something went wrong! \n", error.message)
+      })
   }
 
 }
